5d614680e96b9eebeaf3486472585226d0d3ad5b
Jane <fred.schoeneman@gmail.com>
2023-03-30 17:33:24 -0700

    taiwind not loading

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'components/index'
-
-  root 'components#index'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
 end
