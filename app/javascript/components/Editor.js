b09d967907753f47d4cfe0873f61b962f5922ee3
Jane <fred.schoeneman@gmail.com>
2023-04-03 16:58:35 -0700

    Tutorial for react finished

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,24 +1,29 @@
+/* global window */
+
 import React, { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './Header';
-import EventList from './EventList';
-import { Routes, Route } from 'react-router-dom';
 import Event from './Event';
+import EventForm from './EventForm';
+import EventList from './EventList';
+import { success } from '../helpers/notifications';
+import { handleAjaxError } from '../helpers/helpers';
 
 const Editor = () => {
   const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await window.fetch('/api/events');
+        const response = await window.fetch('/api/events.json');
         if (!response.ok) throw Error(response.statusText);
+
         const data = await response.json();
         setEvents(data);
       } catch (error) {
-        setIsError(true);
-        console.error(error);
+        handleAjaxError(error);
       }
 
       setIsLoading(false);
@@ -27,20 +32,98 @@ const Editor = () => {
     fetchData();
   }, []);
 
+  const addEvent = async (newEvent) => {
+    try {
+      const response = await window.fetch('/api/events.json', {
+        method: 'POST',
+        body: JSON.stringify(newEvent),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) throw Error(response.statusText);
+
+      const savedEvent = await response.json();
+      const newEvents = [...events, savedEvent];
+      setEvents(newEvents);
+      success('Event Added!');
+      navigate(`/events/${savedEvent.id}`);
+    } catch (error) {
+      handleAjaxError(error);
+    }
+  };
+
+  const deleteEvent = async (eventId) => {
+    const sure = window.confirm('Are you sure?');
+
+    if (sure) {
+      try {
+        const response = await window.fetch(`/api/events/${eventId}.json`, {
+          method: 'DELETE',
+        });
+
+        if (!response.ok) throw Error(response.statusText);
+
+        success('Event Deleted!');
+        navigate('/events');
+        setEvents(events.filter(event => event.id !== eventId));
+      } catch (error) {
+        handleAjaxError(error);
+      }
+    }
+  };
+
+  const updateEvent = async (updatedEvent) => {
+    try {
+      const response = await window.fetch(
+        `/api/events/${updatedEvent.id}.json`,
+        {
+          method: 'PUT',
+          body: JSON.stringify(updatedEvent),
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) throw Error(response.statusText);
+
+      const newEvents = events;
+      const idx = newEvents.findIndex((event) => event.id === updatedEvent.id);
+      newEvents[idx] = updatedEvent;
+      setEvents(newEvents);
+
+      success('Event Updated!');
+      navigate(`/events/${updatedEvent.id}`);
+    } catch (error) {
+      handleAjaxError(error);
+    }
+  };
+
   return (
     <>
       <Header />
-      {isError && <p>Something went wrong. Check the console.</p>}
       {isLoading ? (
-        <p>Loading...</p>
+        <p className='loading'>Loading...</p>
       ) : (
-        <>
+        <div className="grid">
           <EventList events={events} />
 
           <Routes>
-            <Route path=":id" element={<Event events={events} />} />
+            <Route
+              path=":id"
+              element={<Event events={events} onDelete={deleteEvent} />}
+            />
+            <Route
+              path=":id/edit"
+              element={<EventForm events={events} onSave={updateEvent} />}
+            />
+            <Route path="new" element={<EventForm onSave={addEvent} />} />
           </Routes>
-        </>
+        </div>
       )}
     </>
   );
